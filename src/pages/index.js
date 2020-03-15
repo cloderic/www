11806137544f69c54fae50767b3cfe0d2b169d89
Clodéric Mars <cloderic.mars@gmail.com>
2020-03-15 19:40:14 +0100

    Introducing images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import Img from 'gatsby-image';
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
+export const query = graphql`
+  query {
+    face: file(relativePath: { eq: "images/cloderic-stationf.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello there!</h1>
+    <p>🚧🚧🚧</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Img fluid={data.face.childImageSharp.fluid} />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>

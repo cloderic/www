6ceff88aa56c73c4afab19d3ed613efbe0edf6ac
Clodéric Mars <cloderic.mars@gmail.com>
2020-03-16 17:39:41 +0100

    Better footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,13 @@ import styled from '@emotion/styled';
 import { CONTAINER_WIDTH } from '../theme/sizes';
 import Stylesheet from '../theme/stylesheet';
 import { WHITE } from '../theme/colors';
+import Link from './link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCreativeCommons,
+  faCreativeCommonsBy,
+  faOsi
+} from '@fortawesome/free-brands-svg-icons';
 
 const Container = styled.div`
   margin: 0 auto;
@@ -22,9 +29,22 @@ const Layout = ({ children }) => (
     <Stylesheet />
     <Main>{children}</Main>
     <Footer>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <p>
+        <FontAwesomeIcon icon={faCreativeCommons} />{' '}
+        <FontAwesomeIcon icon={faCreativeCommonsBy} /> The content on this
+        website, of which Clodéric Mars is the author, is licensed under a{' '}
+        <Link href="https://creativecommons.org/licenses/by/4.0/">
+          Creative Commons Attribution 4.0 International license
+        </Link>
+        .
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faOsi} /> The sources of this website, of which
+        Clodéric Mars is the author, are licensed under a{' '}
+        <Link href="https://choosealicense.com/licenses/mit/">MIT License</Link>{' '}
+        and are available on{' '}
+        <Link href="https://github.com/cloderic/www">Github.</Link>
+      </p>
     </Footer>
   </>
 );

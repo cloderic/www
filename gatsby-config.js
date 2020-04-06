e7f7fe34ba288eb7943f68b0e9a0ac61cb7e6372
Clodéric Mars <cloderic.mars@gmail.com>
2020-04-06 12:54:35 +0200

    Update description

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ const { rgb, parseToRgb } = require('polished');
 
 const siteMetadata = {
   title: `cloderic.com`,
-  description: `Clodéric Mars Portfolio`,
+  description: `Clodéric Mars - Tech Leader, AI Specialist, Public Speaker, humming from Paris`,
   author: 'Clodéric Mars',
   siteUrl: 'https://www.cloderic.com',
   keywords: ['ai', 'tech', 'public speaking'],

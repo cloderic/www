18ee7e660685f67bf2ee20318b4b8b26ffd54a6c
Clodéric Mars <cloderic@hey.com>
2024-04-14 20:54:35 -0400

    test fix 2

--- app/content/[slug]/page.js
+++ app/content/[slug]/page.js
@@ -3,12 +3,12 @@ import { DateTime } from 'luxon';
 import loadContent from '../utils/loadContent';
 import listContent from '../utils/listContent';
 
-async function loadDatedContent({ params }) {
-  const matchingContent = (await listContent()).find(
-    ({ slug }) => slug === params.slug
-  );
-  return await loadContent(matchingContent.path);
-}
+// async function loadDatedContent({ params }) {
+//   const matchingContent = (await listContent()).find(
+//     ({ slug }) => slug === params.slug
+//   );
+//   return await loadContent(matchingContent.path);
+// }
 
 // export async function generateMetadata({ params }) {
 //   const { frontmatter } = await loadDatedContent({ params });
@@ -18,15 +18,16 @@ async function loadDatedContent({ params }) {
 // }
 
 export default async function Page({ params }) {
-  const { content, frontmatter } = await loadDatedContent({ params });
-  const publicationDate =
-    frontmatter.date && DateTime.fromJSDate(frontmatter.date, { zone: 'UTC' });
-  const updateDate =
-    frontmatter.last_update &&
-    DateTime.fromJSDate(frontmatter.last_update, { zone: 'UTC' });
+  // const { content, frontmatter } = await loadDatedContent({ params });
+  // const publicationDate =
+  //   frontmatter.date && DateTime.fromJSDate(frontmatter.date, { zone: 'UTC' });
+  // const updateDate =
+  //   frontmatter.last_update &&
+  //   DateTime.fromJSDate(frontmatter.last_update, { zone: 'UTC' });
   return (
     <>
-      <H1 noanchor>{frontmatter.title}</H1>
+      <div>ahahahahah</div>
+      {/* <H1 noanchor>{frontmatter.title}</H1>
       {frontmatter.date && (
         <small>
           Published on{' '}
@@ -36,7 +37,7 @@ export default async function Page({ params }) {
           .
         </small>
       )}
-      {/* {content} */}
+      {content}
       <footer>
         {updateDate && (
           <p>
@@ -49,7 +50,7 @@ export default async function Page({ params }) {
             </small>
           </p>
         )}
-      </footer>
+      </footer> */}
     </>
   );
 }

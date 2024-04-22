435487c9c54a34a03e762697c8edc3b0b33e795b
Clodéric Mars <cloderic@hey.com>
2024-04-22 13:34:39 -0400

    Add music

---
+++ app/music/page.js
@@ -0,0 +1,36 @@
+import { H1 } from '../../components/base';
+import Link from '../../components/link';
+import HomeLink from '../../components/homeLink';
+import listContent from '../content/utils/listContent';
+import sortBy from 'lodash.sortby';
+
+export const metadata = {
+  title: 'Music',
+  alternates: {
+    canonical: '/content/music'
+  }
+};
+
+export default async function Music() {
+  const content = (
+    await listContent({ parseFrontmatter: true })
+  ).filter(({ categories = [] }) => categories.find((c) => c === 'music'));
+  const sortedContent = sortBy(content, 'date').reverse();
+  return (
+    <>
+      <H1 noanchor>🎸 Music</H1>
+      <ul>
+        {sortedContent.map(({ slug, title, date }, index) => (
+          <li key={index}>
+            <Link href={`/content/${slug}`}>
+              {title} / {date.toFormat('yyyy/MM/dd')}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <footer className="mt-4 text-center">
+        <HomeLink />
+      </footer>
+    </>
+  );
+}
---
+++ components/players/soundCloud.js
@@ -0,0 +1,45 @@
+import { useMemo } from 'react';
+import twConfig from '../../tailwind.config';
+import MediaPlayer from './mediaPlayer';
+
+export default function SoundCloud({
+  trackId,
+  originalHref,
+  downloadHref,
+  ...otherProps
+}) {
+  const url = useMemo(() => {
+    const url = new URL('https://w.soundcloud.com/player/');
+    url.searchParams.append(
+      'url',
+      `https%3A//api.soundcloud.com/tracks/${trackId}`
+    );
+    url.searchParams.append('color', twConfig.theme.colors.pink.DEFAULT);
+    url.searchParams.append('auto_play', false);
+    url.searchParams.append('hide_related', true);
+    url.searchParams.append('show_comments', false);
+    url.searchParams.append('show_user', true);
+    url.searchParams.append('show_reposts', false);
+    url.searchParams.append('show_teaser', false);
+    url.searchParams.append('visual', true);
+    return url;
+  }, [trackId]);
+  return (
+    <MediaPlayer
+      originalLabel="Open in SoundCloud"
+      originalHref={originalHref}
+      downloadHref={downloadHref}
+      {...otherProps}
+    >
+      <iframe
+        width="100%"
+        height="300"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        className="aspect-video"
+        src={url.toString()}
+      ></iframe>
+    </MediaPlayer>
+  );
+}

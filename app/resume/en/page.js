13a6a70037c05f91debe83aeefe93eb7762cf95d
Clodéric Mars <cloderic@hey.com>
2024-04-27 10:52:45 -0400

    WIP Resume

--- app/resume/en/page.js
+++ app/resume/en/page.js
@@ -1,5 +1,9 @@
+import sortBy from 'lodash.sortby';
+
 import loadContent from '../../content/utils/loadContent';
+import listContent from '../../content/utils/listContent';
 import { H1, H2, H3 } from '../../../components/title';
+import ContentList from '../../../components/contentList';
 
 export const metadata = {
   title: 'Resume',
@@ -9,12 +13,46 @@ export const metadata = {
 };
 
 export default async function ResumeEn({}) {
-  const resumeMdx = await loadContent('app/content/resume-en.mdx');
+  const { content, frontmatter } = await loadContent(
+    'app/content/resume-en.mdx'
+  );
+  const contentItems = sortBy(
+    await listContent({ parseFrontmatter: true }),
+    'date'
+  ).reverse();
+  const talks = contentItems.filter(({ categories = [] }) =>
+    categories.find((category) => category == 'talk')
+  );
+  const publications = contentItems.filter(({ categories = [] }) =>
+    categories.find((category) => category == 'publication')
+  );
+  const certifications = contentItems.filter(({ categories = [] }) =>
+    categories.find((category) => category == 'certification')
+  );
   return (
     <>
-      <H1>Clodéric Mars</H1>
-      AI Product Engineer / Tech Leader / Public Speaker
-      {resumeMdx.content}
+      <H1>{frontmatter.title}</H1>
+      {frontmatter.header}
+      {content}
+      <H2>Education</H2>
+      <H3>MOOCS</H3>
+      <ContentList
+        items={certifications}
+        renderDate={({ date }) => `${date.toFormat('yyyy/MM')}`}
+        renderTitle={({ title, short_title, organization }) => (
+          <>
+            <strong>{organization}</strong> - {short_title || title}
+          </>
+        )}
+        renderSubtitle={({ topics = [] }) => topics.join(', ')}
+      />
+      <H2>Talks</H2>
+      <ContentList
+        items={talks}
+        renderDate={({ date }) => date.toFormat('yyyy/MM/dd')}
+        renderTitle={({ short_title, title }) => short_title || title}
+        renderSubtitle={({ venue }) => `@ ${venue}`}
+      />
     </>
   );
 }

2d0dfd093acb3aa11a03fbcac2c899bd280c308f
Clodéric Mars <cloderic@hey.com>
2024-08-15 16:25:44 -0400

    Using formatjs for proper localization

--- components/printButton.js
+++ components/printButton.js
@@ -1,18 +1,22 @@
-'use client';
-
 import clsx from 'clsx';
+import { defineMessage } from '@formatjs/intl';
+import PrintButtonClient from './printButtonClient';
 
-export default function PrintButton({ className, ...otherProps }) {
+export default function PrintButton({ className, intl, ...otherProps }) {
+  const message = defineMessage({
+    id: 'actions.print',
+    defaultMessage: 'Print or save to pdf...'
+  });
+
   return (
-    <button
+    <PrintButtonClient
       className={clsx(
         'bg-pink text-blue py-2 px-4 rounded-full text-sm print:hidden',
         className
       )}
-      onClick={() => print()}
       {...otherProps}
     >
-      Print or save to pdf...
-    </button>
+      {intl.formatMessage(message)}
+    </PrintButtonClient>
   );
 }
